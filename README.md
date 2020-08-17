47ac8b5357d01bc41a2de240a72949d41539f73a
zhouxinyu0723 <58866727+zhouxinyu0723@users.noreply.github.com>
2020-08-17 22:08:18 +0200

    haha
    haha

---
+++ README.md
@@ -0,0 +1,3 @@
+# tryClone
+
+Developed with Unreal Engine 4
---
+++ Source/tryClone/tryCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "tryCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRYCLONE_API AtryCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
